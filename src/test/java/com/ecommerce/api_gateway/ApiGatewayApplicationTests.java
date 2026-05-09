2a48490d32830a4b1b0e5391181163e509f5d3d3
Tushar <tushar.urade@gmail.com>
2026-05-09 21:30:52 +0530

    onboard api gateway

---
+++ src/test/java/com/ecommerce/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ecommerce.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.ecommerce.apigateway.ApiGatewayApplication;
+
+@SpringBootTest(classes = ApiGatewayApplication.class)
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
