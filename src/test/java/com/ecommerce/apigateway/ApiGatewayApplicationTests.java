77430b00e1a3f08a7d32eac6ebf3fb067ecf3585
Tushar <tushar.urade@gmail.com>
2026-05-12 23:42:57 +0530

    rebuild services configuration

--- src/test/java/com/ecommerce/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/ecommerce/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,9 @@
-package com.ecommerce.api_gateway;
+package com.ecommerce.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.ecommerce.apigateway.ApiGatewayApplication;
 
-@SpringBootTest(classes = ApiGatewayApplication.class)
+@SpringBootTest
 class ApiGatewayApplicationTests {
 
 	@Test
